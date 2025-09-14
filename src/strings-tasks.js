09cfc7584d847bdbe7934c63a89c762fb8588eee
Elena Yulova <elenavy@gmail.com>
2025-09-14 11:26:29 +0300

    feat: add countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -293,8 +293,9 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = 'aeiouyAEIOUY';
+  return [...str].filter((char) => vowels.includes(char)).length;
 }
 
 /**
